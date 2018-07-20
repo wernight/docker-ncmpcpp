a822648ff13993e41cac378db12c2ca7a779f8de
Sam Merry <sam@getpolymorph.com>
2018-07-19 18:48:19 -0700

    fixing udp proxy and config

--- ncmpcpp.sh
+++ ncmpcpp.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-udp_fifo() {
-  # Start listening to UDP on port 5555 and create a FIFO queue out of it,
-  # used for visualization. See https://github.com/mopidy/mopidy/issues/775
-  mkfifo /tmp/mpd.fifo
-  while :
-  do
-    nc -kluw 1 0.0.0.0 5555 >/tmp/mpd.fifo 2>/dev/null
-    sleep 3
-  done
+mkfifo /tmp/mpd.fifo || true
+
+udpsink(){
+  setsid nc -kluw 1 127.0.0.1 5555 > /tmp/mpd.fifo 2> /dev/null
 }
 
-udp_fifo &
+udpsink &
 
-# Start actual ncmpcpp
-exec ncmpcpp $*
+
+exec "$@"
+
